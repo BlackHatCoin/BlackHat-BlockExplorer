c9f467f06075390eb0c0a7a8c03fd7149e74f47e
Alex BlackHat <contact@blackhatco.in>
2021-05-23 09:20:46 +0300

    fixes, js cleanup, add masternodes link

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,46 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.card_error').click(function (e) {
         $('.preload').toggleClass('vis');
         $('body').toggleClass('oh');
     });
 
-    $('.hambu').click(function(){
+    $('.hambu').click(function () {
         $('.navleft').toggleClass('vis');
         $('body').toggleClass('oh');
         $('#nav-icon4').toggleClass('open');
     });
-    /*toastr options*/
-    toastr.options = {
-        closeButton: true,
-        "positionClass": "toast-bottom-right",
-        "timeOut": "300000"/*too long timeout for test*/
-    };
-    /*end toastr options*/
-    $('#agree').click(function(){
-        if($(this).prop("checked") == true){
-            $('.login').removeClass('login_dis')
-        }
-        else if($(this).prop("checked") == false){
-            $('.login').addClass('login_dis')
-        }
-    });
-    $('.menu_parent').click(function(){
+    $('.menu_parent').click(function () {
         $(this).toggleClass('open');
     });
 })
 
-$('#terms_link').click(function () {
-    $.scrollTo($('#terms'), 1000, {axis:'y', offset:-140});
-
-
-});
-
-$( window ).on("load", function() {
+$(window).on("load", function () {
     $('body').removeClass('oh');
     $('.preload').removeClass('vis');
 });
-
-
 
 const $menu = $('.menu_parent');
 
@@ -51,5 +28,3 @@ $(document).mouseup(e => {
         $menu.removeClass('open');
     }
 });
-
-
